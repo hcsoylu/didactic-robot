84797bc5b212521bc17fef1e3ea865f1b26e909f
hcsoylu <hsyncn17@gmail.com>
2021-12-26 15:27:42 +0300

    added addItem functionality to reducer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useState } from "react";
+import { addItem } from "../features/todoSlice";
+import { useAppDispatch } from "../store";
 
 const Header = () => {
-  return <div>this is head</div>;
+  const [text, setText] = useState<string>("");
+
+  const dispatch = useAppDispatch();
+
+  const onAddItem = () => {
+    if (!text) {
+      alert("input field is required!!!");
+      return;
+    }
+
+    dispatch(addItem({ text }));
+
+    setText("");
+  };
+
+  return (
+    <div className="header">
+      <input
+        className="task"
+        type="text"
+        value={text}
+        placeholder="Please enter task here"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button className="add-button" onClick={onAddItem}>
+        Add
+      </button>
+    </div>
+  );
 };
 
 export default Header;
--- src/features/todoSlice.ts
+++ src/features/todoSlice.ts
@@ -1,13 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { StateMockData } from "../mockdata";
 import { State } from "../model";
+import { v4 } from "uuid";
+
+export interface addItemPayloadAction {
+  text: string;
+  date?: string;
+}
 
 const initialState: State = StateMockData;
 
 const todoSlice = createSlice({
   name: "todos",
   initialState,
-  reducers: {},
+  reducers: {
+    addItem: (state, action: PayloadAction<addItemPayloadAction>) => {
+      const newTodo = {
+        id: v4(),
+        name: action.payload.text,
+        date: action.payload.date,
+      };
+      state.todo.items.unshift(newTodo);
+    },
+  },
 });
 
 export default todoSlice.reducer;
+export const { addItem } = todoSlice.actions;
