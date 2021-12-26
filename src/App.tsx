e58f62a3919a85a0b5891d9c368483ca2939feeb
hcsoylu <hsyncn17@gmail.com>
2021-12-26 14:44:34 +0300

    clean up for fresh start and created basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import Todos from "./Todos";
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <Todos />;
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Main />
+      </div>
+    </div>
+  );
 }
 
 export default App;
