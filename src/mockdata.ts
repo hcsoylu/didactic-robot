9a7f5747b8482b47870459640ec0219baebec7f4
hcsoylu <hsyncn17@gmail.com>
2021-12-26 15:01:58 +0300

    added mock data

---
+++ src/mockdata.ts
@@ -0,0 +1,55 @@
+export const todoMock = [
+  {
+    id: "1000",
+    name: "Learn Golang",
+  },
+  {
+    id: "1010",
+    name: "Learn Rust",
+  },
+  {
+    id: "1020",
+    name: "Learn SvelteKit",
+  },
+];
+
+export const doingMock = [
+  {
+    id: "2000",
+    name: "Learn React Native",
+  },
+  {
+    id: "2010",
+    name: "Learn Flutter",
+  },
+];
+
+export const doneMock = [
+  {
+    id: "3000",
+    name: "Learn React ",
+  },
+  {
+    id: "3010",
+    name: "Learn HTML",
+  },
+  {
+    id: "3020",
+    name: "Learn CSS",
+  },
+];
+
+export const StateMockData = {
+  todo: {
+    title: "Todo",
+    items: todoMock,
+  },
+  doing: {
+    title: "Doing",
+    items: doingMock,
+  },
+  done: {
+    title: "Done",
+    items: doneMock,
+  },
+};
